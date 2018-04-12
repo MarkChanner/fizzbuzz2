d9e6441475f6dd53643eb0942d2f087d69d8d7d2
Mark Channer <markchanner@gmail.com>
2018-04-12 15:35:33 +0100

    Adds tests

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,31 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+
+  it 'returns "fizz" when passed 6' do
+    expect(6.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 10' do
+    expect(10.fizzbuzz).to eq 'buzz'
+  end
+
+  it 'returns 7 when passed 7' do
+    expect(7.fizzbuzz).to eq 7
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(30.fizzbuzz).to eq 'fizzbuzz'
+  end
+end
